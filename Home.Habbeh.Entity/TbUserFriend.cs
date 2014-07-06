b8334fde53ef8d5f748c494f3970a3d1fb779bc5
roomezonline <roomezonline@gmail.com>
2014-07-05 23:23:58 -0700

    Edit ViewQuery And Add Picture,Firstname and last name , Loct Edit TbUserFriend
    ViewUserFriend Script:
    
    SELECT     dbo.TbUserFriend.UserId, TbUser_1.UserName,
                              (SELECT     UserName
                                FROM          dbo.TbUser
                                WHERE      (Id = dbo.TbUserFriend.FriendId)) AS FriendUserName, dbo.TbUserFriend.FriendId, dbo.TbUserFriend.Accept, dbo.TbUserFriend.Id,
                          TbUser_1.Status, TbUser_1.Picture, TbUser_1.FirstName, TbUser_1.LastName, TbUser_1.Location
    FROM         dbo.TbUserFriend INNER JOIN
                          dbo.TbUser AS TbUser_1 ON dbo.TbUserFriend.UserId = TbUser_1.Id

--- Home.Habbeh.Entity/TbUserFriend.cs
+++ Home.Habbeh.Entity/TbUserFriend.cs
@@ -12,6 +12,11 @@ namespace Home.Habbeh.Entity
         public int FriendId { get; set; }
         public String FriendUserName { get; set; }
         public String Accept { get; set; }
+        public String Status { get; set; }
+        public String Picture { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Location { get; set; }
         
         
 
@@ -27,6 +32,11 @@ namespace Home.Habbeh.Entity
             userfriend.FriendId = Convert.ToInt32(reader["FriendId"]);
             userfriend.FriendUserName = reader["FriendUserName"].ToString();
             userfriend.Accept = reader["Accept"].ToString();
+            userfriend.Status = reader["Status"].ToString();
+            userfriend.Picture = reader["Picture"].ToString();
+            userfriend.FirstName = reader["FirstName"].ToString();
+            userfriend.LastName = reader["LastName"].ToString();
+            userfriend.Location = reader["Location"].ToString();
             
             
 
